f76e45ba21a00dc39c8c653147a1ed0ef9d250ed
le_woudar <rollandkev@yahoo.fr>
2022-03-21 12:15:53 +0100

    ci(pre-commit): replaced yapf by black in the config file
    installed dev package black and removed yapf
    formatted code with black

--- ws/commands/tail.py
+++ ws/commands/tail.py
@@ -44,12 +44,9 @@ async def main(filename: str, lines_count: int, follow: bool) -> None:
 
 @click.command()
 @click.argument('filename', type=click.Path(exists=True, dir_okay=False))
-@click.option('-n',
-              'lines_count',
-              type=click.IntRange(min=1),
-              default=10,
-              show_default=True,
-              help='number of lines to print')
+@click.option(
+    '-n', 'lines_count', type=click.IntRange(min=1), default=10, show_default=True, help='number of lines to print'
+)
 @click.option('-f', '--follow', is_flag=True)
 async def tail(filename: str, lines_count: int, follow: bool):
     """
