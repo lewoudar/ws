aa88561f41aae5889344841fdddbd614f55d85b6
le_woudar <rollandkev@yahoo.fr>
2022-03-22 20:37:20 +0100

    feat(settings): implemented a new module settings.py
    This module defines common settings to configure websocket client and server
    pydantic is used to be able to pull values from env variables
    I also defined a function to be able to get configuration from pyproject.toml

---
+++ tests/test_settings.py
@@ -0,0 +1,86 @@
+from ws.settings import Settings, get_config_from_toml, get_settings
+
+
+def test_should_check_default_setting_values():
+    settings = Settings()
+    assert settings.connect_timeout == 5.0
+    assert settings.disconnect_timeout == 5.0
+    assert settings.response_timeout == 5.0
+
+
+def test_should_read_values_from_environment(monkeypatch):
+    monkeypatch.setenv('WS_CONNECT_TIMEOUT', '1')
+    monkeypatch.setenv('ws_disconnect_timeout', '3')
+    settings = Settings()
+
+    assert settings.connect_timeout == 1.0
+    assert settings.disconnect_timeout == 3.0
+    assert settings.response_timeout == 5.0
+
+
+class TestGetConfigFromToml:
+    """Tests function get_config_from_toml"""
+
+    def test_should_return_none_when_ws_config_is_not_present(self, tmp_path):
+        content = '[tool.isort]\n' 'profile = "black"\n'
+        toml_file = tmp_path / 'pyproject.toml'
+        toml_file.write_text(content)
+
+        assert get_config_from_toml(toml_file) is None
+
+    def test_should_return_none_if_file_is_not_a_valid_toml_file(self, tmp_path):
+        file = tmp_path / 'pyproject.toml'
+        file.write_text('Cameroon is a beautiful country!')
+
+        assert get_config_from_toml(file) is None
+
+    def test_should_return_data_when_ws_config_is_present(self, tmp_path):
+        content = (
+            '[tool.isort]\n' 'profile = "black"\n' '[tool.ws]\n' 'connect_timeout=4.0\n' 'disconnect_timeout=3.0\n'
+        )
+        toml_file = tmp_path / 'pyproject.toml'
+        toml_file.write_text(content)
+
+        assert get_config_from_toml(toml_file) == {'connect_timeout': 4.0, 'disconnect_timeout': 3.0}
+
+
+class TestGetSettings:
+    """Tests function get_settings"""
+
+    def test_should_return_default_settings_when_config_file_does_not_exist(self):
+        settings = get_settings()
+
+        assert settings == Settings()
+
+    def test_should_return_default_settings_when_config_file_does_not_contain_ws_config(self, tmp_path, mocker):
+        mocker.patch('pathlib.Path.cwd', return_value=tmp_path)
+        content = 'hello = "world"\n'
+        config_file = tmp_path / 'pyproject.toml'
+        config_file.write_text(content)
+        settings = get_settings()
+
+        assert settings == Settings()
+
+    def test_should_return_settings_with_values_pulled_from_config_file(self, tmp_path, mocker):
+        mocker.patch('pathlib.Path.cwd', return_value=tmp_path)
+        content = '[tool.ws]\n' 'connect_timeout=3.0\n' 'response_timeout=4.0\n' 'fake_setting=2\n'
+        config_file = tmp_path / 'pyproject.toml'
+        config_file.write_text(content)
+        settings = get_settings()
+
+        assert settings.connect_timeout == 3.0
+        assert settings.disconnect_timeout == 5.0
+        assert settings.response_timeout == 4.0
+
+    def test_should_check_function_prioritizes_config_file_over_environment(self, tmp_path, monkeypatch, mocker):
+        mocker.patch('pathlib.Path.cwd', return_value=tmp_path)
+        monkeypatch.setenv('WS_CONNECT_TIMEOUT', '2')
+        content = '[tool.ws]\n' 'connect_timeout=3.0\n' 'response_timeout=4.0\n' 'fake_setting=2\n'
+        config_file = tmp_path / 'pyproject.toml'
+        config_file.write_text(content)
+        settings = get_settings()
+
+        # connect_timeout must be 3 and not 2
+        assert settings.connect_timeout == 3.0
+        assert settings.disconnect_timeout == 5.0
+        assert settings.response_timeout == 4.0
---
+++ ws/settings.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Optional, Union
+
+import tomli
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    connect_timeout: float = 5.0
+    disconnect_timeout: float = 5.0
+    response_timeout: float = 5.0
+
+    class Config:
+        env_prefix = 'ws_'
+
+
+def get_config_from_toml(filename: Union[str, Path]) -> Optional[dict[str, float]]:
+    try:
+        with open(filename, 'rb') as f:
+            data = tomli.load(f)
+    except tomli.TOMLDecodeError:
+        return
+
+    if 'tool' in data and 'ws' in data['tool']:
+        return data['tool']['ws']
+
+
+def get_settings() -> Settings:
+    pyproject_file = Path.cwd() / 'pyproject.toml'
+    settings = Settings()
+
+    if pyproject_file.exists():
+        config = get_config_from_toml(pyproject_file)
+        if config is None:
+            return settings
+
+        for item in Settings.__fields__.keys():
+            if item in config:
+                setattr(settings, item, config[item])
+
+    return settings
