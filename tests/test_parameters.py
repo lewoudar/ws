68816dcf5fee801423f033a5101a8fd2467f744c
le_woudar <rollandkev@yahoo.fr>
2022-03-23 11:38:49 +0100

    feat(parameters): implemented function get_normalized_message and parameter ByteParamType

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,7 +1,7 @@
 import asyncclick as click
 import pytest
 
-from ws.parameters import WsUrlParamType
+from ws.parameters import ByteParamType, WsUrlParamType, get_normalized_message
 
 
 class TestWsUrlParamType:
@@ -9,8 +9,9 @@ class TestWsUrlParamType:
 
     @pytest.mark.parametrize('url', ['ws://websocket', 'https://websocket.com'])
     def test_should_raise_error_when_given_value_is_not_a_websocket_url(self, url):
+        param = WsUrlParamType()
+
         with pytest.raises(click.BadParameter) as exc_info:
-            param = WsUrlParamType()
             param.convert(url, None, None)
 
         assert f'{url} is not a valid websocket url' == str(exc_info.value)
@@ -19,3 +20,64 @@ class TestWsUrlParamType:
     def test_should_return_websocket_url(self, url):
         param = WsUrlParamType()
         assert param.convert(url, None, None) == url
+
+
+class TestGetNormalizedMessage:
+    """Tests function get_normalized_message"""
+
+    @pytest.mark.parametrize(('value', 'is_bytes', 'expected'), [('hello', False, 'hello'), ('hello', True, b'hello')])
+    def test_should_return_given_string(self, value, is_bytes, expected):
+        assert get_normalized_message(value, is_bytes) == expected
+
+    @pytest.mark.parametrize('is_bytes', [True, False])
+    def test_should_raise_error_when_file_does_not_exist(self, is_bytes):
+        with pytest.raises(click.BadParameter) as exc_info:
+            get_normalized_message('file@foo.txt', is_bytes)
+
+        assert 'file foo.txt does not exist' == str(exc_info.value)
+
+    @pytest.mark.parametrize('is_bytes', [True, False])
+    def test_should_raise_error_when_file_cannot_be_opened(self, tmp_path, is_bytes):
+        dummy_file = tmp_path / 'file.txt'
+        dummy_file.write_text('Hello world')
+        dummy_file.chmod(0o333)
+
+        with pytest.raises(click.BadParameter) as exc_info:
+            get_normalized_message(f'file@{dummy_file}', is_bytes)
+
+        assert f'file {dummy_file} cannot be opened' == str(exc_info.value)
+
+    @pytest.mark.parametrize(
+        ('is_bytes', 'expected'),
+        [(False, 'We have the best food in Cameroon'), (True, b'We have the best food in Cameroon')],
+    )
+    def test_should_return_file_content_given_correct_input(self, tmp_path, expected, is_bytes):
+        dummy_file = tmp_path / 'file.txt'
+        dummy_file.write_text('We have the best food in Cameroon')
+
+        assert get_normalized_message(f'file@{dummy_file}', is_bytes) == expected
+
+
+class TestByteParamType:
+    """Tests parameter ByteParamType"""
+
+    def test_should_raise_error_when_value_length_is_greater_than_given_max_length(self):
+        value = 'a' * 101
+        param = ByteParamType(max_length=100)
+
+        with pytest.raises(click.BadParameter) as exc_info:
+            param.convert(value, None, None)
+
+        assert f'{value} is longer than 100 bytes' == str(exc_info.value)
+
+    def test_should_return_bytes_given_string_as_input(self):
+        param = ByteParamType()
+        assert param.convert('hello', None, None) == b'hello'
+
+    def test_should_return_bytes_given_file_as_input(self, tmp_path):
+        value = 'Cameroon is a great country!'
+        dummy_file = tmp_path / 'file.txt'
+        dummy_file.write_text(value)
+        param = ByteParamType()
+
+        assert param.convert(f'file@{dummy_file}', None, None) == value.encode()
--- ws/parameters.py
+++ ws/parameters.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import AnyStr, Optional
 
 import asyncclick as click
 from pydantic import AnyUrl, BaseModel, ValidationError
@@ -22,6 +22,37 @@ class WsUrlParamType(click.ParamType):
             return value
         except ValidationError:
             self.fail(f'{value} is not a valid websocket url', param, ctx)
+
+
+def get_normalized_message(message: str, is_bytes: bool) -> AnyStr:
+    if message.startswith('file@'):
+        file = message[5:]
+        mode = 'rb' if is_bytes else 'r'
+        try:
+            with open(file, mode) as f:
+                return f.read()
+        except FileNotFoundError:
+            raise click.BadParameter(f'file {file} does not exist')
+        except OSError:
+            raise click.BadParameter(f'file {file} cannot be opened')
+    else:
+        return message.encode() if is_bytes else message
+
+
+class ByteParamType(click.ParamType):
+    name = 'bytes'
+
+    def __init__(self, max_length: int = None):
+        self._max_length = max_length
+
+    def convert(self, value: str, param: Optional[click.Parameter], ctx: Optional[click.Context]) -> bytes:
+        original_value = value
+        value = get_normalized_message(value, is_bytes=True)
+        if self._max_length is not None:
+            if value and len(value) > self._max_length:
+                self.fail(f'{original_value} is longer than {self._max_length} bytes')
+
+        return value
 
 
 WS_URL = WsUrlParamType()
