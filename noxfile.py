cdb885a2e5956bf1492037ee17608de17de880ed
le_woudar <rollandkev@yahoo.fr>
2022-05-27 00:05:24 +0200

    ci: installed nox dev package and creates a noxfile.py
    Also created a .coverage.yml to prepare coverage report on codecov

---
+++ noxfile.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+
+import nox
+
+nox.options.reuse_existing_virtualenvs = True
+
+PYTHON_VERSIONS = ['pypy3', '3.7', '3.8', '3.9', '3.10']
+CI_ENVIRONMENT = 'GITHUB_ACTIONS' in os.environ
+
+
+@nox.session(python=PYTHON_VERSIONS[-1])
+def lint(session):
+    """Performs pep8 and security checks."""
+    source_code = 'ws'
+    session.install('flake8==3.9.2', 'bandit==1.7.4')
+    session.run('flake8', source_code)
+    session.run('bandit', '-r', source_code)
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def tests(session):
+    """Runs the test suite."""
+    session.install('poetry>=1.0.0,<2.0.0')
+    session.run('poetry', 'install')
+    session.run('pytest')
+
+
+@nox.session(python=PYTHON_VERSIONS[-1])
+def docs(session):
+    """Builds the documentation."""
+    session.install('mkdocs==1.0.4')
+    session.run('mkdocs', 'build', '--clean')
+
+
+@nox.session(python=False)
+def deploy(session):
+    """
+    Deploys on pypi.
+    """
+    if 'POETRY_PYPI_TOKEN_PYPI' not in os.environ:
+        session.error('you must specify your pypi token api to deploy your package')
+
+    session.install('poetry>=1.0.0,<2.0.0')
+    session.run('poetry', 'publish', '--build')
+
+
+@nox.session(python=False)
+def clean(*_):
+    """Since nox take a bit of memory, this command helps to clean nox environment."""
+    shutil.rmtree('.nox', ignore_errors=True)
